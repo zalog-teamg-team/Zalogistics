d2f0df641066c3869b4cba2d11b9736a3e69f3e6
Zalog-hung <zalogfinal@gmail.com>
2025-10-24 15:36:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // var/www/html/config/config.js
+//test
 window.GAS_URL =
   "https://script.google.com/macros/s/AKfycbyX6ipz7UA0LwDJKlMvRaRXfzo0R1BP_GMO2PaRlQAYcxxhnDdC7CO6DSduhrMDIhZ8Fw/exec";
 window.SAVE_URL_BASE = "/api/filejson/"; //
