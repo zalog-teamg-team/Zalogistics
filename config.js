a4d96c5c91293663ad20967baeb2973d8827f20a
Zalog-hung <zalogfinal@gmail.com>
2025-10-24 17:20:52 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// var/www/html/config/config.js
-//test
-window.GAS_URL =
-  "https://script.google.com/macros/s/AKfycbyX6ipz7UA0LwDJKlMvRaRXfzo0R1BP_GMO2PaRlQAYcxxhnDdC7CO6DSduhrMDIhZ8Fw/exec";
-window.SAVE_URL_BASE = "/api/filejson/"; //
-window.DATA_BASE_URL = "/filejson/";
